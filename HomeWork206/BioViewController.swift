b4791775837f33a469d66638d5d85f7eb9dc787e
XXNNDDRR <xndrgo@gmail.com>
2023-12-02 21:52:57 +0300

    Final Again

--- HomeWork206/BioViewController.swift
+++ HomeWork206/BioViewController.swift
@@ -1,15 +1,20 @@
+//
+//  BioViewController.swift
+//  HomeWork206
+//
+//  Created by Alexander on 02.12.2023.
+//
+
 import UIKit
 
 final class BioViewController: UIViewController {
-        
+
     @IBOutlet weak var labelBio: UILabel!
     var user: User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        labelBio.text = user.person.bio.bio
 
+        labelBio.text = user.person.bio.bio
     }
-    
 }
